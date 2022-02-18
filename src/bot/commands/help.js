723e709980fd2c22071c776c565fae8392fac8a3
LightBlueGamer <69762409+LightBlueGamer@users.noreply.github.com>
2022-02-18 21:55:50 +0100

    Fixed help command

--- src/bot/commands/help.js
+++ src/bot/commands/help.js
@@ -6,12 +6,12 @@ module.exports = {
     name: "help",
     description: "Shows available commands.",
     async execute(message, args) {
-        const { prefix } = getConfig(message.guild.id);
+        const { prefix } = await getConfig(message.guild.id);
         const commands = message.client.commands;
         
         const embed = new MessageEmbed()
         .setTitle(`Commands`)
-        .setDescription(commands.map(command => `${prefix}${command.name}: ${command.description}`).join("/n"))
+        .setDescription(commands.map(command => `${prefix}${command.name}: ${command.description}`).join("\n"))
         return message.reply({
             embeds: [embed]
         });
