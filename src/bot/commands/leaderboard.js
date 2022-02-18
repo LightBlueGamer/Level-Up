3250b750f97a3fabbcab306bad7bad9ab1d30f18
LightBlueGamer <69762409+LightBlueGamer@users.noreply.github.com>
2022-02-18 20:12:21 +0100

    added leaderboard

---
+++ src/bot/commands/leaderboard.js
@@ -0,0 +1,55 @@
+const { config, profile } = require("../../db/index");
+const { getProfile } = require("../modules/profile.js");
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: "leaderboard",
+    description: "Shows the leaderboard",
+    async execute(message, args) {
+        const users = [];
+        const arg1 = args[1];
+
+        for(const key of await (profile.keys)) {
+            const { balance, bank, prestige, xp, level } = await getProfile(key);
+            const user = await message.client.users.fetch(key);
+
+            users.push({
+                name: user.username,
+                id: key,
+                xp,
+                level,
+                prestige,
+                balance,
+                bank
+            });
+        };
+
+        let page = !arg1 ? 1 : isNaN(arg1) ? 1 : parseInt(arg1);
+        if(page*10>users.length) page = Math.ceil(users.length/10);
+        if(page <= 0) page = 1;
+        const pageIdx = page - 1;
+
+        const sorted = users.sort((a, b) => {
+            if(a.total < b.total) return 1;
+            else if(a.total > b.total) return -1;
+            else return 0;
+        });
+
+        const top10 = sorted.slice(pageIdx * 10, pageIdx * 10 + 10);
+
+        const embed = new MessageEmbed()
+            .setTitle(`${message.guild.name} top ${pageIdx * 10 + 10}`)
+            .setDescription(`Page ${page}/${Math.ceil(users.length/10)}`)
+            .setColor(message.member.roles.highest.hexColor)
+            .setFooter({
+                text: `You are #${sorted.findIndex(x => x.id === message.author.id)+1} out of ${sorted.length}.`
+            })
+            for(let i=0; i<top10.length; i++) {
+                const ind = top10[i];
+                embed.addField(`#${sorted.indexOf(ind)+1} ${ind.name}`,`Balance: ${ind.balance} - Bank: ${ind.bank}\nPrestige: ${ind.prestige} - Level: ${ind.level} - Exp: ${ind.xp}`)
+            };
+            return message.reply({
+                embeds: [embed]
+            });
+    },
+};
