c397a559eaff6c001a801b88479b1d253d61f624
LightBlueGamer <69762409+LightBlueGamer@users.noreply.github.com>
2022-02-19 11:29:15 +0100

    Possibly made leaderboard embed nicer

--- src/bot/commands/leaderboard.js
+++ src/bot/commands/leaderboard.js
@@ -52,7 +52,8 @@ module.exports = {
             })
             for(let i=0; i<top10.length; i++) {
                 const ind = top10[i];
-                embed.addField(`#${sorted.indexOf(ind)+1} ${ind.name}`,`Balance: $${ind.balance} - Bank: $${ind.bank}\nPrestige: ${ind.prestige} - Level: ${ind.level} - Exp: ${ind.xp}`)
+                embed.addField(`#${sorted.indexOf(ind)+1} ${ind.name}`,`Balance: $${ind.balance} - Bank: $${ind.bank}\nPrestige: ${ind.prestige} - Level: ${ind.level} - Exp: ${ind.xp}`, true)
+                if(i % 2 === 1) embed.addField(`\u200b`, `\u200b`, true)
             };
             return message.reply({
                 embeds: [embed]
